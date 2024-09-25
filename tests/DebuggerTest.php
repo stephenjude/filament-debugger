fd1d7be3959e4765640ea6be81afcc0e35f03b0b
stephenjude <stephenjudesuccess@gmail.com>
2024-09-25 11:52:35 +0100

    added navigation group test

--- tests/DebuggerTest.php
+++ tests/DebuggerTest.php
@@ -14,18 +14,25 @@ it('registers plugin', function () {
         ->toThrow(Exception::class);
 });
 
-it('groups plugin navigation items', function () {});
+it('groups plugin navigation items', function () {
+    $plugin = DebuggerPlugin::make()
+        ->navigationGroup(condition: fn() => true, label: 'Laravel Debuggers');
+
+    expect($plugin)->getNavigationGroupLabel()->toBe('Laravel Debuggers');
+
+    expect($plugin)->hasNavigationGroup()->toBeTrue();
+});
 
 it('customizes telescope navigation', function () {
     $plugin = DebuggerPlugin::make()
         ->horizonNavigation(false)
         ->pulseNavigation(false)
         ->telescopeNavigation(
-            condition: fn () => true,
+            condition: fn() => true,
             label: 'Laravel Telescope',
             icon: 'heroicon-o-users',
             url: 'telescope/requests',
-            openInNewTab: fn () => false
+            openInNewTab: fn() => false
         );
 
     expect($plugin->hasHorizon())->toBeFalse();
@@ -48,11 +55,11 @@ it('customizes horizon navigation', function () {
         ->telescopeNavigation(false)
         ->pulseNavigation(false)
         ->horizonNavigation(
-            condition: fn () => true,
+            condition: fn() => true,
             label: 'Laravel Horizon',
             icon: 'heroicon-o-users',
             url: url('horizon/requests'),
-            openInNewTab: fn () => false
+            openInNewTab: fn() => false
         );
 
     expect($plugin->hasPulse())->toBeFalse();
@@ -75,11 +82,11 @@ it('customizes pulse navigation', function () {
         ->horizonNavigation(false)
         ->telescopeNavigation(false)
         ->pulseNavigation(
-            condition: fn () => true,
+            condition: fn() => true,
             label: 'Laravel Pulse',
             icon: 'heroicon-o-users',
             url: 'pulse/requests',
-            openInNewTab: fn () => false
+            openInNewTab: fn() => false
         );
 
     expect($plugin->hasHorizon())->toBeFalse();
